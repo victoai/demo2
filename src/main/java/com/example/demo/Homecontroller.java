9699fc6af147cc008b1350b854e322e088a84ab3
jooyeon <victoai999@gmail.com>
2024-06-28 19:25:33 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/Homecontroller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Homecontroller {
+
+
+    @ResponseBody
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+    }
+}
